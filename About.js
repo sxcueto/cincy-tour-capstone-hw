6b0e0170b3e5f6ff449e6d85374f006435c5d554
unknown <sxcueto@gmail.com>
2023-06-06 18:45:49 -0400

    push for hw updates

--- About.js
+++ About.js
@@ -1,35 +1,35 @@
 import React from "react";
 import { FaArrowDown, FaArrowUp, FaLinkedin, FaLink, FaGithub } from 'react-icons/fa';
 import FunFact from './FunFact';
-
-const About = () => {
-    return (
-        <div className="scroll-container">
-            {/* //section one "about" landing page */}
-            <section id="section-one">
-                <header className="container-fluid" id="jumbo">
-                    <div className="container-fluid text-center">
-                        <br />
-                        <a href="/about#section-two">
-                            <FaArrowDown className="icons" />
-                        </a>
-                    </div>
-                </header>
-            </section>
-            <br />
-
-            { /* section two: info about Cincy */}
-            <section id="section-two">
-                <div className="container-fluid text-center" id="content-two">
-                    <a href="/about#section-one">
-                        <FaArrowUp className="icons" />
-                    </a>
-                    <br /> <br />
-                    {/* <h3>Cincinnati</h3> */}
-                    <p>Known for it's spaghetti chili, flying pigs, and world-famous hippos, Cincinnati is the place to be to experience a variety of cultures, art, food, and history. Click the dice below to learn some fun facts about our city. </p>
+import Button from "react-bootstrap/esm/Button";
+const About = () =>{
+return (
+    <div>
+        {/* //section one "about" landing page */}
+        <section id="section-one">
+            <header className="container-fluid" id="about">
+                <div className="container-fluid text-center">
+                    <div className="display-7" id="first-icons">
+                    <a href="https://www.artworkscincinnati.org/mural/the-hands-that-built-our-city/" target="_blank" rel="noopener noreferrer"><Button variant="info"><i className="bi bi-info-lg"></i></Button></a>
                     <br />
+                    <a href="/about#section-two">
+                        <FaArrowDown className="icons" />
+                    </a>
+                   </div> 
+                   </div>
+                    </header>
+                    </section>
+                    <br /> <br />
+
+            {/* section two about Cincinnati */}
+        <section id="section-two">
+        <div className="container-fluid text-center">
+                <a href="/about#section-one">
+                    <FaArrowUp className="icons" />
+                    </a>
+                    <p id="content-two">Known for it's spaghetti chili, flying pigs, and world-famous hippos, Cincinnati is the place to be to experience a variety of cultures, art, food, and history. Click the dice below to learn some fun facts about our city. </p>
                     {/* dice for fun fact generator*/}
-                    <div><FunFact /></div>
+                    <div id= "dice" className="container-fluid justify-content"><FunFact /></div>
                     <a href="/about#section-three">
                         <FaArrowDown className="icons" />
                     </a>
@@ -39,51 +39,55 @@ const About = () => {
 
             {/* //section three: info about group */}
             <section id="section-three">
-                <div className="row row-cols-sm g-0 container-fluid text-center" id="card-group">
+                <div className="container-fluid text-center" id="card-group">
+                <div className="row"><div className="col-12 text-center">
                     <a href="/about#section-two">
                         <FaArrowUp className="icons" />
                     </a>
                     <h2 className="team-header">Capstone Team</h2>
-                    <br />
-                    <div className="card h-40 w-25" id="card-inside">
-                        <img className="card-img-top" src="/" alt="..." />
+                    <br /></div></div><div className="row">
+                    <div className="col-lg-3"></div>
+                    <div className="col-lg-2">
+                    <div className="card h-100" id="card-inside">
+                        <img className="card-img-top h-150" src="https://drive.google.com/uc?export=view&id=1yiMe4SE_qay6ZMM0NINVa2OAFqp8HjpQ" alt="Eliane Bettocchi" />
                         <div className="card-body">
                             <h5 className="card-title">Eliane Bettocchi</h5>
                             <br />
                             <div className="container" id="social">
-                                <a href="https://www.linkedin.com/in/lilith/">
+                                <a href="https://www.linkedin.com/in/lilith/" target="_blank">
                                     <FaLinkedin className="icons" />
                                 </a>
-                                <a href="https://github.com/elianebettocchi">
+                                <a href="https://github.com/elianebettocchi" target="_blank">
                                     <FaGithub className="icons" />
                                 </a>
-                                <a href="/">
+                                <a href="https://elianebettocchi.github.io/portfolio/" target="_blank">
                                     <FaLink className="icons" />
                                 </a>
                             </div>
                         </div>
-                    </div>
-                    <div className="card h-40 w-25" id="card-inside">
-                        <img className="card-img-top" src="/" alt="..." />
+                    </div></div>
+                    <div className="col-lg-2">
+                    <div className="card h-100" id="card-inside">
+                        <img className="card-img-top h-200" src="https://drive.google.com/uc?export=view&id=1WObYakmif0wZOnsc240I1dZ1ojJHXtJ5" alt="Stephanie Cueto" />
                         <div className="card-body">
                             <h5 className="card-title">Stephanie Cueto</h5>
                             <br />
                             <div className="container" id="social">
-                                <a href="https://www.linkedin.com/in/stephanie-cueto/">
+                                <a href="https://www.linkedin.com/in/stephanie-cueto/" target="_blank">
                                     <FaLinkedin className="icons" />
                                 </a>
-                                <a href="https://github.com/sxcueto">
+                                <a href="https://github.com/sxcueto" target="_blank">
                                     <FaGithub className="icons" />
                                 </a>
-                                <a href="/">
+                                <a href="https://sxcueto.github.io/portfolio2/" target="blank">
                                     <FaLink className="icons" />
                                 </a>
                             </div>
                         </div>
-                    </div>
-
-                    <div className="card h-40 w-25" id="card-inside">
-                        <img className="card-img-top" src="/" alt="..." />
+                    </div></div>
+                    <div className="col-lg-2">
+                    <div className="card h-100" id="card-inside">
+                        <img className="card-img-top img-fluid" src="/" alt="Robert Head" />
                         <div className="card-body">
                             <h5 className="card-title">Robert Head</h5>
                             <br />
@@ -91,7 +95,7 @@ const About = () => {
                                 <a href="/">
                                     <FaLinkedin className="icons" />
                                 </a>
-                                <a href="https://github.com/PinusStrobus77">
+                                <a href="https://github.com/PinusStrobus77" target="_blank">
                                     <FaGithub className="icons" />
                                 </a>
                                 <a href="/">
@@ -100,7 +104,7 @@ const About = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div></div></div>
             </section>
 
             <br />
--- FunFact.js
+++ FunFact.js
@@ -1,3 +1,5 @@
+//worked on styling modal box and the icons in the generator
+
 import React, { useState } from "react";
 import { FaDiceD20 } from 'react-icons/fa'
 import Modal from "react-bootstrap/Modal";
@@ -6,11 +8,14 @@ const facts = [
   'Cincinnati was originally named Losantville. It was renamed in 1790 in honor of Lucius Cincinnatus. He was a military leader and statesman in Rome who lived from 519 to 430 B.C.E.',
   'Cincinnati is the birthplace of Neil Armstrong. He was born in Cincinnati in August 1930. After his victorious career with NASA, he went on to teach aeronautical engineering at the University of Cincinnati from 1971 to 1979.',
   'The Cincinnati Observatory was established in 1842 and is the oldest professional observatory in the country and in the entire western hemisphere.',
+  'The Cincinnati Observatory was established in 1842 and is the oldest professional observatory in the country and in the entire western hemisphere.',
   'Cincinnati was a key stop in the Underground Railroad. Slaves escaping from the south could stop here on their way to Canada to be free. You can learn more on this subject at the National Underground Railroad Freedom Center right here in Cincinnati.',
   'Cincinnati is home to the biggest Oktoberfest in the country and second largest in the world with Germany holding the number one spot.',
   'Founded in 1895, the Cincinnati Symphony Orchestra is the sixth oldest orchestra in the country.',
   'Cincinnati is the home to the only ventriloquist museum in the world. The museum is called Vent Haven Museum and can only be visited via appointment.',
   'The Cincinnati Reds was the first Major League Baseball team in the country. Established in 1890, the team was originally named the Cincinnati Red Stockings.',
+  'Cincinnati is known as The Queen City but do you know its other nicknames? It is also known as The City of Seven Hills, The Queen of the West, The Blue Chip City, and Porkopolis.',
+  'Cincinnati is known for its chili that includes cinnamon and chocolate in its seasoning. It is usually served on spagetti or hot dogs and piled high with cheese.', 
   'Cincinnati is known as The Queen City but do you know its other nicknames? It is also known as The City of Seven Hills, The Queen of the West, The Blue Chip City, and Porkopolis.',
   'Cincinnati is known for its chili that includes cinnamon and chocolate in its seasoning. It is usually served on spagetti or hot dogs and piled high with cheese.', 
   'Cincinnati is "the capital of Cornhole". Locals gave rise to the party game starting back in 2005.'
@@ -24,13 +29,17 @@ const FunFact = () => {
     const randomNumber = Math.floor(Math.random() * (facts.length));
     setDisplayedFact(facts[randomNumber]);
     setShow(true);
+    setDisplayedFact(facts[randomNumber]);
+    setShow(true);
   }
   
+  
   return (
-  <div>
+  <div className="container-fluid justify-content">
     <Button variant="primary" onClick={handleShow}>
       <FaDiceD20 />
     </Button>
+ 
     <Modal id="modal-box"show={show} onHide={handleClose}>
       <Modal.Header className="text-right">
         <Button variant="tertiary" id="x-button" onClick={handleClose}>X</Button>
@@ -42,10 +51,12 @@ const FunFact = () => {
           <FaDiceD20 />
         </Button>
         </div>
+     
       </Modal.Footer>
     </Modal>
   </div>
   )
+  
 }
 
 export default FunFact;
